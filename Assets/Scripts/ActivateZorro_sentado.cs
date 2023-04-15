f127ea4001b211c733ca35dc1ef74b11936c2d10
guiraigumi <63981480+guiraigumi@users.noreply.github.com>
2023-04-16 01:04:44 +0200

    v10

--- Assets/Scripts/ActivateZorro_sentado.cs
+++ Assets/Scripts/ActivateZorro_sentado.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ActivateZorro : MonoBehaviour
+public class ActivateZorro_sentado : MonoBehaviour
 {
     public GameObject zorro1;
     private bool triggered = false;
@@ -8,6 +8,9 @@ public class ActivateZorro : MonoBehaviour
     public GameObject Cam;
     public GameObject otherGameObject;
     public GameObject chest;
+    public GameObject collider1;
+    public GameObject collider2;
+    public GameObject collider3;
 
     private void Start()
     {
@@ -19,10 +22,10 @@ public class ActivateZorro : MonoBehaviour
         if (other.gameObject.layer == playerLayer && !triggered)
         {
             zorro1.SetActive(true);
-            triggered = true; 
+            triggered = true;
             Cam.gameObject.SetActive(true);
-            Invoke("DeactivateZorro", 6f);
-            
+            Invoke("DeactivateZorro", 4f);
+
         }
     }
 
@@ -32,8 +35,10 @@ public class ActivateZorro : MonoBehaviour
         zorro1.SetActive(false);
         otherGameObject.SetActive(true);
         chest.SetActive(true);
+        Destroy(collider1.gameObject);
+        Destroy(collider2.gameObject);
+        Destroy(collider3.gameObject);
         Destroy(this.gameObject);
+        
     }
 }
-
-
